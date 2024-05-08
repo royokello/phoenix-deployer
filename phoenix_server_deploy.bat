8365f25c73b3ef6fafea802856766fa5bed0f298
Roy <royokello@protonmail.com>
2024-05-08 20:01:42 +0300

    ft: updates to new deployment procedure

--- phoenix_server_deploy.bat
+++ phoenix_server_deploy.bat
@@ -1,20 +1,31 @@
 @echo off
 
-REM Load environment variables from file
+echo Deleting packed file...
+del deploy.tar.gz
+
+echo Delete compilation directory...
+rmdir /S /Q deploy_tar
+
+echo Create compilation directory...
+IF NOT EXIST deploy_tar mkdir deploy_tar
+
+echo Compiling files...
+robocopy . deploy_tar /E /XD "deps" "_build" "node_modules" ".git" ".elixir_ls" ".github"
+
+echo Packing files...
+tar -czvf deploy.tar.gz -C deploy_tar .
+
+echo Loading environment variables ...
 if not exist "%env_file%" (
-    echo Error: Environment file "%env_file%" not found.
+    echo Error: Environment file "%env_file%" not found!
     exit /b 1
 )
 
 for /f "delims=" %%a in (%env_file%) do set %%a
 
-REM Check if all required variables are set
-if "%USERNAME%"=="" goto :missing_username
-if "%KEY_PATH%"=="" goto :missing_key_path
-if "%SERVER_ADDRESS%"=="" goto :missing_server_address
-
-REM Check if private key is encrypted
+echo Checking if private key is encrypted ...
 set is_encrypted=false
+
 for /f "tokens=1* delims=:" %%A in ('ssh-keygen -y -f "%KEY_PATH%" 2^>nul') do (
     if "%%A"=="-----BEGIN ENCRYPTED PRIVATE KEY-----" (
         set is_encrypted=true
@@ -23,73 +34,35 @@ for /f "tokens=1* delims=:" %%A in ('ssh-keygen -y -f "%KEY_PATH%" 2^>nul') do (
 
 REM If key is encrypted, prompt for passphrase
 if "%is_encrypted%"=="true" (
-    set /p passphrase="Enter passphrase for private key: "
+    set /p passphrase="enter passphrase for private key: "
 )
 
-REM Execute SSH command
-if "%is_encrypted%"=="true" (
-    sshpass -p %passphrase% ssh -i "%KEY_PATH%" %USERNAME%@%SERVER_ADDRESS% %*
-) else (
-    ssh -i "%KEY_PATH%" %USERNAME%@%SERVER_ADDRESS% %*
-)
-goto :eof
+echo Stopping service...
+ssh %SERVER_USER%@%SERVER_IP% sudo systemctl stop phoenix.service
 
-:missing_username
-echo Error: Username is not set in the environment file.
-exit /b 1
+echo Uploading files...
+scp deploy.tar.gz %SERVER_USER%@%SERVER_IP%:%SERVER_DIR%
 
-:missing_key_path
-echo Error: Private key path is not set in the environment file.
-exit /b 1
+echo Deleting packed file...
+del deploy.tar.gz
 
-:missing_server_address
-echo Error: Server address is not set in the environment file.
-exit /b 1
+echo Delete compilation directory...
+rmdir /S /Q deploy_tar
 
+echo Unpacking files...
+ssh %SERVER_USER%@%SERVER_IP% "rm -rf phoenix; mkdir -p phoenix; tar -xzvf deploy.tar.gz -C phoenix"
 
+echo Downloading phoenix-server-start.sh from GitHub...
+ssh %SERVER_USER%@%SERVER_IP% "sudo wget -O /root/phoenix/phoenix_server_start.sh https://raw.githubusercontent.com/royokello/phoenix-server-start/main/phoenix_server_start.sh; sudo chmod +x /root/phoenix/phoenix_server_start.sh"
+echo phoenix_server_start.sh downloaded.
 
+echo Downloading phoenix.service from GitHub...
+ssh %SERVER_USER%@%SERVER_IP% "sudo wget -O /etc/systemd/system/phoenix.service https://raw.githubusercontent.com/royokello/phoenix-service-file/main/phoenix.service; sudo systemctl daemon-reload"
+echo phoenix.service.
 
-
-
-
-
-
-
-
-REM -- Change these variables as per your requirement --
-SET "ZIP_FILE=phoenix_app.tar.gz"
-SET "SOURCE_DIR=."
-set DEST_DIR=/phoenix
-set SERVER_USER=root
-set SERVER_IP=178.128.36.156
-set SERVER_SERVICE=phoenix
-
-REM -- Zip the necessary files, excluding certain directories --
-echo Zipping necessary files...
-tar -czvf %ZIP_FILE% %SOURCE_DIR% --exclude=deps --exclude=_build --exclude=node_modules --exclude=.git
-
-REM -- Stop the server service --
-echo Stopping the server service...
-ssh %SERVER_USER%@%SERVER_IP% sudo systemctl stop %SERVER_SERVICE%
-
-REM -- Transfer the zip file --
-echo Transferring files...
-scp %ZIP_FILE% %SERVER_USER%@%SERVER_IP%:%DEST_DIR%
-
-REM -- Delete old app directory, unzip new files, and restart service --
-echo Setting up on server...
-ssh %SERVER_USER%@%SERVER_IP% "
-    rm -rf %DEST_DIR%
-    unzip %DEST_DIR%/%ZIP_FILE% -d %DEST_DIR%
-    cd %DEST_DIR%
-    mix deps.get --only prod
-    MIX_ENV=prod mix compile
-    npm install --prefix ./assets
-    npm run deploy --prefix ./assets
-    mix phx.digest
-    mix ecto.migrate
-    sudo systemctl start %SERVER_SERVICE%
-"
+echo Starting phoenix.service...
+ssh %SERVER_USER%@%SERVER_IP% sudo systemctl start phoenix.service
+echo phoenix.service started.
 
 echo Process completed
 pause
